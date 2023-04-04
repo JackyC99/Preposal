5f1dd3684addb9cc0ac43841f16aaf12cb4ba46e
JackyC99 <129879709+JackyC99@users.noreply.github.com>
2023-04-04 11:48:34 -0400

    Add files via upload

---
+++ Preposal.py
@@ -0,0 +1,16 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+A tree growing simulation where there is a list of things we can do to help
+the tree grow and there would be events that may or maynot benefit in.The tree
+can die if something went wrong.
+
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+just me
+
+## Do you have any questions of your own?
+
+none
